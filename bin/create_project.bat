3cbf7454c7642d2d5da0e451126699103a7eb571
David Lakatos <dlakatos847@gmail.com>
2014-02-09 17:39:20 +0000

    Cleaning repository

--- bin/create_project.bat
+++
@@ -1,6 +0,0 @@
-
-set JPF_CREATE_HOME=%~dp0..
-
-set JVM_FLAGS=-Xmx1024m -ea
-
-java %JVM_FLAGS% -classpath "%JPF_CREATE_HOME%\build\jpf-template.jar;%JPF_CREATE_HOME%\lib\*" gov.nasa.jpf.template.CreateProject %*
