18b303647a9628e0f1c05a62ef12c48de9d57fd7
David Lakatos <dlakatos847@gmail.com>
2014-03-03 15:26:37 +0000

    Restorable state experiment

--- src/main/hu/bme/mit/ftsrg/jointstates/listener/JointstatesInterveningListener.java
+++ src/main/hu/bme/mit/ftsrg/jointstates/listener/JointstatesInterveningListener.java
@@ -3,7 +3,9 @@ package hu.bme.mit.ftsrg.jointstates.listener;
 import gov.nasa.jpf.JPF;
 import gov.nasa.jpf.ListenerAdapter;
 import gov.nasa.jpf.search.Search;
+import gov.nasa.jpf.vm.ChoiceGenerator;
 import gov.nasa.jpf.vm.Instruction;
+import gov.nasa.jpf.vm.RestorableVMState;
 import gov.nasa.jpf.vm.ThreadInfo;
 import gov.nasa.jpf.vm.VM;
 
@@ -11,53 +13,71 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class JointstatesInterveningListener extends ListenerAdapter {
-  static Logger logger = JPF.getLogger(JointstatesInterveningListener.class.getCanonicalName());
+	static Logger logger = JPF.getLogger(JointstatesInterveningListener.class
+			.getCanonicalName());
+	static List<RestorableVMState> restorableStates = new ArrayList<RestorableVMState>();
 
-  /*
-   * Make sure jpf-jointstates master-JPF is available (non-Javadoc)
-   * @see gov.nasa.jpf.ListenerAdapter#searchStarted(gov.nasa.jpf.search.Search)
-   */
-  @Override
-  public void searchStarted(Search search) {
-    // TODO Auto-generated method stub
-    // sendHeartbeatRequest();
-    super.searchStarted(search);
-  }
+	/*
+	 * Make sure jpf-jointstates master-JPF is available (non-Javadoc)
+	 * 
+	 * @see
+	 * gov.nasa.jpf.ListenerAdapter#searchStarted(gov.nasa.jpf.search.Search)
+	 */
+	@Override
+	public void searchStarted(Search search) {
+		// TODO Auto-generated method stub
+		// sendHeartbeatRequest();
+		super.searchStarted(search);
+	}
 
-  @Override
-  public void executeInstruction(VM vm, ThreadInfo currentThread, Instruction instructionToExecute) {
-    // TODO Auto-generated method stub
-    // if(instructionToExecute instanceof gov.nasa.jpf.jvm.bytecode.)
-    super.executeInstruction(vm, currentThread, instructionToExecute);
+	@Override
+	public void executeInstruction(VM vm, ThreadInfo currentThread,
+			Instruction instructionToExecute) {
+		// TODO Auto-generated method stub
+		// if(instructionToExecute instanceof gov.nasa.jpf.jvm.bytecode.)
+		super.executeInstruction(vm, currentThread, instructionToExecute);
+	}
 
-  }
+	@Override
+	public void choiceGeneratorSet(VM vm, ChoiceGenerator<?> newCG) {
+		super.choiceGeneratorSet(vm, newCG);
+		restorableStates.add(vm.getRestorableState());
+	}
 
-  public void sendHeartbeatRequest() {
-    String hostName = "127.0.0.1";
-    Socket socket = null;
-    OutputStream outStream = null;
-    try {
-      InetAddress addr = InetAddress.getByName(hostName);
-      int portNumber = 7000;
-      socket = new Socket(addr, portNumber);
-      outStream = socket.getOutputStream();
-      outStream.write(99);
-      if (socket.getInputStream().read() == 100) {
-        logger.info("GOOD HEARTBEAT RESPONSE");
-      } else {
-        logger.severe("WRONG HEARTBEAT RESPONSE");
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      try {
-        socket.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-  }
+	@Override
+	public void searchFinished(Search search) {
+		super.searchFinished(search);
+		logger.info("Restorable states: " + restorableStates.size());
+	}
+
+	public void sendHeartbeatRequest() {
+		String hostName = "127.0.0.1";
+		Socket socket = null;
+		OutputStream outStream = null;
+		try {
+			InetAddress addr = InetAddress.getByName(hostName);
+			int portNumber = 7000;
+			socket = new Socket(addr, portNumber);
+			outStream = socket.getOutputStream();
+			outStream.write(99);
+			if (socket.getInputStream().read() == 100) {
+				logger.info("GOOD HEARTBEAT RESPONSE");
+			} else {
+				logger.severe("WRONG HEARTBEAT RESPONSE");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
